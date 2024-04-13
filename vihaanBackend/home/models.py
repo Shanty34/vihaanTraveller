d13e874d8be38f8d388b5f0509127f77b83d8ed7
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-04-13 11:27:50 +0530

    Mongo Setting

--- vihaanBackend/home/models.py
+++ vihaanBackend/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    personality = models.CharField(max_length=30)
